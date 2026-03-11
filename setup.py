b39e110c7666f6708d8970cabeacd93767eb2e2a
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-11 16:50:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="EggTools",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="A simple Python library that lets you update and install my modules, scan python code for potential malicious code and crash python.",
     author="",
